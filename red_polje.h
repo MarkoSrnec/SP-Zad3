e3bdd07b00001cc169dceb35dcffd7f52c62b73c
MarkoSrnec <srnec.marko8@gmail.com>
2014-12-14 21:31:57 +0100

    Create red_polje.h
    Implementacija reda pomocu cirkularnog polja

---
+++ red_polje.h
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+struct klijent{
+       int ai, bi, ci, di;
+       char ime[30], prezime[30];
+       int dan, mj, god;
+       char spol;     
+};
+struct q {
+       klijent klijenti[10000];
+       int front,rear;
+};
+typedef struct q red;
+ 
+int AddOne(int n) {
+    return((n+1)%10000);
+}
+bool IsEmptyQ(red *Q){
+     if(AddOne(Q->rear)==Q->front) 
+     return true;
+     else
+     return false;
+     
+     }
+klijent FrontQ(red *Q){
+        if(!IsEmptyQ(Q))
+             return Q->klijenti[Q->front];
+        
+                     
+}
+void EnQueueQ(klijent X, red *Q){
+     if(AddOne(AddOne(Q->rear))!=Q->front){
+          Q->rear=AddOne(Q->rear);                               
+          Q->klijenti[Q->rear]=X;     
+     }
+     else {
+           
+           return;
+     }
+}
+void DeQueueQ(red *Q){
+     if(!IsEmptyQ(Q))
+         Q->front=AddOne(Q->front);
+     else {
+           
+           return;
+     }          
+}
+ 
+red *InitQ(red *Q){
+    Q=new red;
+    Q->front=0;
+    Q->rear=9999;
+    return Q;   
+}
